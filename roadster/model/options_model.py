fa78ffbbef0b9a7550eabc93ad7148c0393a43cc
Christofanis Skordas <skordasc@uchicago.edu>
2026-09-21 09:30:43 -0500

    switches to IDDModel

--- roadster/model/options_model.py
+++ roadster/model/options_model.py
@@ -1,5 +1,5 @@
 from qtpy import QtCore
-from roadster.model.config.bmc.bmc_model import BMCModel
+from roadster.model.config.idd.idd_model import IDDModel
 
 ORGANIZATION = "GSECARS"
 APPNAME = "Roadster"
@@ -12,4 +12,4 @@ class OptionsModel:
 
         self.app_settings = QtCore.QSettings("settings.ini", QtCore.QSettings.IniFormat)
 
-        self.station = BMCModel()
+        self.station = IDDModel()
